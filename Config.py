c055695b22e7f0103b869e52a275ed9f606af540
Nedal Swehli <53152702+nswehli@users.noreply.github.com>
2021-01-26 09:03:25 -0500

    Delete Config.py

--- Config.py
+++
@@ -1,6 +0,0 @@
-
-
-consumer_key = "LL7sHUk9V7Hlus8MNjwT0PWek"
-consumer_secret = "AR8itLZ73ZZ3Iyc5ohqnAUVqHIs4PY3ByQ9MGlSNm2MIamXbtf"
-access_token = "1263955342540582917-Pk7gAeBZqszWS8WQiTWvmBLNYvdfv2"
-access_token_secret = "FYGFUr5SWhqZBokTXwsJwtciUVHL1S56TuycW7U7QY2R8"
